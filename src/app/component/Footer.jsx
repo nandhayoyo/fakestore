d9970f7e4de71a34d5d5b948735acc228f9a97f8
nandhayoyo <nandha.yuana2@Gmail.com>
2023-10-10 20:13:27 +0700

    plus : resource for image ui

--- src/app/component/Footer.jsx
+++ src/app/component/Footer.jsx
@@ -2,6 +2,8 @@
 
 import Link from "next/link";
 import { toast } from "react-hot-toast";
+import people1 from "../assets/image/people2.png";
+import Image from "next/image";
 
 export default function Footer() {
   const handleClick = (e) => {
@@ -13,23 +15,14 @@ export default function Footer() {
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
-          <div className="mb-6 md:mb-0">
-            <Link href="/" className="flex items-center">
-              <img
-                src={`/assets/images/logo_n.png`}
-                className="h-8 mr-3"
-                alt="logo"
-              />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap text-green-800 dark:text-white">
-                Fakestore
-              </span>
-            </Link>
-          </div>
+          <Image width="auto" src={people1} className="mr-3" alt="logo" />
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                Resources
-              </h2>
+              <Link href="/" className="flex items-center">
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                  Fakestore
+                </h2>
+              </Link>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li className="mb-4">
                   <Link
